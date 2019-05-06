9fe4d5434ad9668eac1d6a8a9e1e9342d07d4564
Sandeep Shrestha <sandeep.c.shrestha@gmail.com>
2019-05-05 19:21:03 -0500

    firebase setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyBqa3kZFcoKULuBWTK07qJGD64cqfI81nQ",
+            authDomain: "auth-38f33.firebaseapp.com",
+            databaseURL: "https://auth-38f33.firebaseio.com",
+            projectId: "auth-38f33",
+            storageBucket: "auth-38f33.appspot.com",
+            messagingSenderId: "543300136354",
+            appId: "1:543300136354:web:16670fb6311934f4"
+        });
+    }
+
     render() {
         return (
             <View>
