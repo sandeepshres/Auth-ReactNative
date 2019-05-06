8195e32cfba38f167a86bcca273aaddfb5069367
Sandeep Shrestha <sandeep.c.shrestha@gmail.com>
2019-05-06 15:20:34 -0500

    created reusable inputtext

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/loginForm';
 
 class App extends Component {
     componentWillMount() {
@@ -20,9 +21,9 @@ class App extends Component {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>
-                    An App!!
-                </Text>
+                <LoginForm>
+
+                </LoginForm>
             </View>
         );
 
---
+++ src/components/loginForm.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Card, CardSection, Button, Input } from './common';
+
+class LoginForm extends Component {
+    state = { email: '' };
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        placeholder="abc@gmail.com"
+                        label="Email"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })} />
+                </CardSection>
+
+                <CardSection>
+
+                </CardSection>
+
+                <CardSection>
+                    <Button>
+                        Login
+                    </Button>
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+export default LoginForm;
